ba28c61abc19b1828ae05300df2217a9f423753c
Kazuhiro Kojio <kazuhiro.kojio@gmail.com>
2023-07-04 15:39:30 -0300

    refactor: optimize extractor

--- scripts/extractor.py
+++ scripts/extractor.py
@@ -1,18 +1,17 @@
 import pandas as pd
 
 def extractor(rules_table):
-  organized_rules = pd.DataFrame(columns = ('items_base', 'items_add', 'support', 'confidence', 'lift'))
-  count = 0
-  index = 0
+  organized_rules = []
 
-  while count < rules_table.shape[0]:
+  for count in range(rules_table.shape[0]):
     for rule in rules_table.iloc[count, 2]:
-      organized_rules.loc[index, 'items_base'] = list(rule.items_base)
-      organized_rules.loc[index, 'items_add'] = list(rule.items_add)
-      organized_rules.loc[index, 'support'] = rules_table.iloc[count, 1]
-      organized_rules.loc[index, 'confidence'] = rule.confidence
-      organized_rules.loc[index, 'lift'] = rule.lift
-      index += 1
-    count += 1
-  
-  return organized_rules
+      organized_rule = {
+        'items_base': list(rule.items_base),
+        'items_add': list(rule.items_add),
+        'support': rules_table.iloc[count, 1],
+        'confidence': rule.confidence,
+        'lift': rule.lift
+      }
+      organized_rules.append(organized_rule)
+
+  return pd.DataFrame(organized_rules, columns=['items_base', 'items_add', 'support', 'confidence', 'lift'])
